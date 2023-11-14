5cac9f55d5afda476dfed23fcdd0c77f07057b41
IamGatheru <davidgatheru490@gmail.com>
2023-11-13 18:46:38 -0800

    Betty

--- _printf.c
+++ _printf.c
@@ -1,66 +1,59 @@
 #include "main.h"
+/**
+ * _printf - produces output according to a format
+ * @format:  is a character string
+ * Return: the string excluding the null byte.
+ */
 
 int _printf(const char *format, ...)
 {
-    int chara_print = 0;
-    va_list list_of_args;
+	int chara_print;
+	va_list list_of_args;
 
-    if (format == NULL)
-        return (-1);
+	chara_print = 0;
 
-    va_start(list_of_args, format);
+	if (format == NULL)
+		return (-1);
 
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            write(1, format, 1);
-            chara_print++;
-        }
-        else  
-        {
-            format++;
-            if (*format == '\0')
-                break;
-                
-            if (*format == '%')
-            {
-                write (1, format,1);
-                chara_print++;
-            }
-            else if (*format == 'c')
-            {
-                char c = va_arg(list_of_args, int);
-                write(1,&c,1);
-                chara_print++;
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(list_of_args, char*);
-                int str_len = 0;
-                while (str[str_len] != '\0')
-                    str_len++;
-                    
-                write(1, str, str_len);
-                chara_print += str_len;           
-            }
-        }
+	va_start(list_of_args, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			chara_print++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				chara_print++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(list_of_args, int);
 
-        format++;
-    }
-    va_end (list_of_args);
+				write(1, &c, 1);
+				chara_print++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(list_of_args, char*);
+				int str_len = 0;
 
-    return chara_print;
+				while (str[str_len] != '\0')
+					str_len++;
+				write(1, str, str_len);
+				chara_print += str_len;
+			}
+		}
+		format++;
+	}
+	va_end(list_of_args);
+
+	return (chara_print);
 }
-
-int main(void)
-{
-    _printf("David\n");
-    _printf("%%\n");
-    _printf("%c\n", "H");
-    _printf("%s\n", "Show me sth");
-
-    return (0);
-}
-
-
